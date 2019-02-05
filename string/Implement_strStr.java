b4869480789c902557b2e6dc03b6e91df066456c
Aayers-ghw <1515182041@qq.com>
2019-02-05 23:02:13 +0800

    2019/2/5

--- string/Implement_strStr.java
+++ string/Implement_strStr.java
@@ -2,4 +2,24 @@ package string;
 
 public class Implement_strStr {
 
+	//BF
+	public int strStr(String haystack, String needle) {
+        if(needle == null)  return 0;
+        int i = 0;
+        int j = 0;
+        while(i < haystack.length() && j < needle.length()){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                i++;
+                j++;
+            }else{
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+        if(j >= needle.length()){
+            return i - needle.length();
+        }else{
+            return -1;
+        }
+    }
 }
